05a0fc1fd22bad49ca4dcb20b4604511f196cd46
andrsd <andrsd@330ec0f2-1f52-0410-893d-495b1d6c41c7>
2008-08-20 09:17:56 +0000

    Code clean-up

--- richesseGPS/RichesseGPS.cpp
+++ richesseGPS/RichesseGPS.cpp
@@ -39,65 +39,6 @@ static char THIS_FILE[] = __FILE__;
 #endif
 
 
-#if 0
-/////////////////////////////////////////////////////////////////////////////
-// CAboutDlg dialog used for App About
-
-class CAboutDlg : public CCeDialog {
-public:
-	CAboutDlg();
-
-// Dialog Data
-	//{{AFX_DATA(CAboutDlg)
-	enum { IDD = IDD_ABOUTBOX };
-	CLinkCtrl	m_ctlHomePage;
-	//}}AFX_DATA
-
-	// ClassWizard generated virtual function overrides
-	//{{AFX_VIRTUAL(CAboutDlg)
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-	//}}AFX_VIRTUAL
-
-// Implementation
-protected:
-	//{{AFX_MSG(CAboutDlg)
-	virtual BOOL OnInitDialog();		// Added for WCE apps
-	//}}AFX_MSG
-	DECLARE_MESSAGE_MAP()
-};
-
-CAboutDlg::CAboutDlg() : CCeDialog(CAboutDlg::IDD) {
-	//{{AFX_DATA_INIT(CAboutDlg)
-	//}}AFX_DATA_INIT
-	SetMenu(IDR_CANCEL);
-}
-
-void CAboutDlg::DoDataExchange(CDataExchange* pDX) {
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CAboutDlg)
-	DDX_Control(pDX, IDC_HOMEPAGE, m_ctlHomePage);
-	//}}AFX_DATA_MAP
-}
-
-BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
-	//{{AFX_MSG_MAP(CAboutDlg)
-		// No message handlers
-	//}}AFX_MSG_MAP
-END_MESSAGE_MAP()
-
-/////////////////////////////////////////////////////////////////////////////
-// CRichesseGPSApp commands
-// Added for WCE apps
-
-BOOL CAboutDlg::OnInitDialog()  {
-	CCeDialog::OnInitDialog();
-
-	return TRUE;  // return TRUE unless you set the focus to a control
-	              // EXCEPTION: OCX Property Pages should return FALSE
-}
-#endif
-
 /////////////////////////////////////////////////////////////////////////////
 // CRichesseGPSApp
 
